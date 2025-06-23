8c6106446a967d54c894d579340b3703f1e522d9
mpollak <marcellopollak123@gmail.com>
2025-06-23 11:09:27 -0400

    modified film.js

--- films/film.js
+++ films/film.js
@@ -8,4 +8,27 @@ function parseFilmQuery(){
     return id;
 }
 
+async function querySWAPI(){
+
+    //get query param for film id from client's URL
+    const id = parseFilmQuery()
+
+    //attempts to query the SWAPI endpoint
+    try{
+        const response = await fetch(`http://localhost:9001/api/films/${id}`)
+        if (!response.ok) {
+            throw new Error(`HTTP Error! status: ${response.status}`);
+        }
+        //collecting the JSON response
+        const data = await response.json();
+        console.log(data)
+
+    } catch (error){
+        console.error('There was a problem with the fetch operation: ', error);
+    }
+}
+
+querySWAPI()
+     
+
 
